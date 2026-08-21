f0d99c679a47a228d2508cf9605e92096f955d3c
Vansh <vanshj746@gmail.com>
2026-08-21 19:59:48 +0530

    Add Simple Interest Calculator

---
+++ Project 2 Simple Interest.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void)
+{
+    float a;
+    printf("Enter the principal amount:\n");
+    scanf("%f", &a);
+
+    float b;
+    printf("Enter number of years:\n");
+    scanf("%f", &b);
+
+    float c;
+    printf("Enter interest rate(in %%):\n");
+    scanf("%f", &c);
+
+    printf("Value of simple interest is %.2f", (a*b*c)/100);
+
+
+
+
+
+
+
+
+
+
+}
